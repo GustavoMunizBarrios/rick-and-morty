f62fc1a69402c2366bff56601c71f3b8f1c2ed58
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2023-04-06 13:20:04 -0500

    agregue dos nuevos componentes: Detail y About

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import './App.css'
-import Cards from './components/Cards';
-import Nav from "./components/Nav";
+import './App.css';
+import Cards from './components/Cards.jsx';
+import Nav from './components/Nav';
+import About from './components/About';
+import Detail from './components/Detail';
 import { useState } from 'react';
 import axios from 'axios';
+import { Routes, Route } from 'react-router-dom';
 
+const URL_BASE = 'https://be-a-rym.up.railway.app/api/character';
+const API_KEY = 'f99ef8399d40.cea0bfba7c15f21eb580';
 
 function App() {
-    const [characters, setCharacters] = useState([]); //characters es un estado del tipo array de objetos
-
+   const [characters, setCharacters] = useState([]); //characters es un estado del tipo array de objetos
 
    const onSearch = (id) => {
       //Con axios le hago peticiones a una api al servidor, el id lo estamos obteniendo del input, 
       //es decir de lo que escribe el usuario. Una vez que tiene una respuesta en el .then 
 
       //Axios retorna un objeto con una propiedad llamada data y ahi dentro es donde tengo la información de la api
-      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+      axios(`${URL_BASE}/${id}?key=${API_KEY}`)
+      .then(response => response.data)
+      .then((data) => {
          if (data.name) {
             setCharacters((oldChars) => [...oldChars, data]);
          } else {
-            alert('¡No hay personajes con este ID!');
+            window.alert('¡No hay personajes con este ID!');
          }
       });
    }
 
    const onClose = (id) => {
-      const charactersFiltered = characters.filter(character => 
-         character.id !== Number(id)) // si da true se queda si da false se va el character
-         setCharacters(charactersFiltered)
+      const charactersFiltered = characters.filter(character => character.id !== Number(id))// si da true se queda si da false se va el character
+      setCharacters(charactersFiltered)
    }
 
-    return(
-        <div className='App'>
-            <Nav onSearch={onSearch}/>
-            <Cards characters={characters} onClose={onClose}/>
-                
-        </div>
-    )
+   return (
+      <div className='App'>
+         <Nav onSearch={onSearch} />
+
+         <Routes>
+            <Route path='/home' element={ <Cards characters={characters} onClose={onClose}/> }/>
+            <Route path='/about' element={<About/>} />
+            <Route path='/detail/:id' element={<Detail/>} />
+         </Routes>
+        
+      </div>
+   );
 }
+
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,20 +1,16 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function SearchBar({ onSearch }) {
-
-   const[id,setId] = useState('');
+   const [id, setId] = useState('');
 
    const handleChange = (event) => {
-      setId(event.target.value) // event.target.value extrae el valor del input       
+      setId(event.target.value)
    }
 
-   //onChange detecta cambios en el input y manda llamar a la función handleChange
-   // value= {id} guarda el value (lo que escrivimos en el input) en el estado local id 
-   //()=> onSearch(id) le estamos pasando el estado id como argumento cuando ejecutamos la función onSearch
    return (
       <div>
-         <input type='search' onChange={handleChange} value={id}/>
-         <button onClick={()=> {onSearch(id); setId('')} }>Agregar</button>
+         <input type='search' onChange={handleChange} value={id} />
+         <button onClick={() =>{onSearch(id); setId('')}}>Agregar</button>
       </div>
    );
 }
